d3fb18940b88679dafef615602c6514afac5954a
sorachung <sorachung.src@gmail.com>
2022-06-12 16:58:54 -0500

    added solution to isPalindrome

--- Program.cs
+++ Program.cs
@@ -70,6 +70,25 @@
 
         return prefix;
       }
+
+      //https://leetcode.com/problems/palindrome-number/
+      public static bool IsPalindrome(int x)
+      {
+        if (x < 0)
+        {
+          return false;
+        }
+
+        String str = x.ToString();
+        for (int i = 0; i < str.Length / 2; i++)
+        {
+          if (str[i] != str[str.Length - 1 - i])
+          {
+            return false;
+          }
+        }
+        return true;
+      }
     }
   }
 }
